617ce377c74594b138728ab496b87bf0c0efa231
Enkhtaivan <mir18.enktaivan@gmail.com>
2022-03-28 17:25:50 +0800

    8.component-communication

---
+++ 8.Component-communication/cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeElement.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- <li>
+    <h3>{{ topicName }}</h3>
+    <p>{{ description }}</p>
+    <button @click="$emit('select-topic', id)">Learn More</button>
+  </li> -->
+  <li>
+    <h3>{{ topic.title }}</h3>
+    <p>{{ topic.description }}</p>
+    <button @click="$emit('select-topic', topic.id)">Learn more me</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['id', 'topicName', 'description', 'topic'],
+  emits: ['select-topic'],
+};
+</script>
